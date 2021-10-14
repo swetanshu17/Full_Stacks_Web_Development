9e386be3d3d0d2cb49b51537f1890f5581db3455
swetanshu17 <swetanshusbp@gmail.com>
2021-10-14 06:50:41 +0530

    primitive data-types

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+console.warn("Hello Warning")
+console.error("Hello error")
+
+/*Data Types*/
+
+// String
+console.log(typeof"Hello World")
+console.log(typeof"d")
+
+//Number
+console.log(typeof 12325262626262626241161)
+console.log(typeof 1)
+console.log(typeof 1.23)
+console.log(typeof -13)
+console.log(typeof -1.234)
+
+// Boolean
+console.log(typeof true)
+console.log(typeof false)
