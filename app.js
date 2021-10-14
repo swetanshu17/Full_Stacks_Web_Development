cde0a94fdcdc88050f8e39001ee23481684fa85f
swetanshu17 <swetanshusbp@gmail.com>
2021-10-14 07:02:18 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-console.log("Hello World")
-console.warn("Hello Warning")
-console.error("Hello error")
+var age = null
 
-/*Data Types*/
-
-// String
-console.log(typeof"Hello World")
-console.log(typeof"d")
-
-//Number
-console.log(typeof 12325262626262626241161)
-console.log(typeof 1)
-console.log(typeof 1.23)
-console.log(typeof -13)
-console.log(typeof -1.234)
-
-// Boolean
-console.log(typeof true)
-console.log(typeof false)
+// console.log(Name)
+console.log(typeof age)
